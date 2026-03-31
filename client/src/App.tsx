e025cf432be6ff64134ad9b7c55febbcaa6df969
surajagrawal01 <surajagrawal70500@gmail.com>
2026-03-31 16:21:33 +0530

    chore: initial project setup (client + server with base architecture)

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Resumable File Upload System</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
